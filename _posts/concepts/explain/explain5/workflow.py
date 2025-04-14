da95c4db20d3f5f1a95581c3c933f58b9b908038
samrat kar <samratk@gmail.com>
2025-04-14 08:50:02 +0530

    init

--- _posts/concepts/explain/explain5/workflow.py
+++ _posts/concepts/explain/explain5/workflow.py
@@ -9,13 +9,35 @@ from llama_index.core.workflow import (
     Context
 )
 from llama_index.utils.workflow import draw_all_possible_flows
+from llama_index.core.workflow import Event
 """--------------------------------------------------------------------------------"""
+# defining the events
+class FirstEvent(Event):
+    first_output: str
+
+class SecondEvent(Event):
+    second_output: str
+
+# defining the workflows
 class MyWorkflow(Workflow):
     # declare a function as a step
     @step
-    async def my_step(self, ev: StartEvent) -> StopEvent:
+    async def step_one(self, ev: StartEvent) -> FirstEvent:
         # do something here
-        return StopEvent(result="Hello, world!")
+        #print (ev.first_input)
+        #print (ev.first_output)
+        return FirstEvent(first_output="First step complete!")
+    
+    @step
+    async def step_two(self, ev: FirstEvent) -> SecondEvent:
+        # do something here
+        # print (ev.first_input)
+        return SecondEvent(second_output="Second step complete!")
+
+    @step
+    async def step_three(self, ev: SecondEvent) -> StopEvent:
+        # do something here
+        return StopEvent(third_output="Third step complete!")
 
 
 async def main():
@@ -25,7 +47,6 @@ async def main():
     result = await basic_workflow.run()
     print(result)
     draw_all_possible_flows(basic_workflow, filename="C:/github/samratkar.github.io/_posts/concepts/explain/explain5/workflows/basic_workflow.html")
-
 """--------------------------------------------------------------------------------"""
 
 if __name__ == "__main__":
