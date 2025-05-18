0cc479975c6eafc1123ec3616ad2186bfa11fc72
samrat kar <samratk@gmail.com>
2025-05-18 11:16:41 +0530

    init

---
+++ _posts/concepts/agentic_ai_2_0/installations/cmds.bat
@@ -0,0 +1,34 @@
+:: commands for conda 
+conda create -n agentic_2_base python=3.11
+conda activate agentic_2_base
+conda env list
+
+:: different path
+:: present working directory as path is not given only the folder name is given as bacd001
+:: any location path can be given in the command
+:: -n is for default location. -p for new location.
+conda create -p abcd001 python=3.11
+conda activate <path>
+
+:: install from a requirement.txt file
+pip install -r requirements.txt 
+
+:: create requirement.txt file 
+pip freeze > requirement.txt
+
+
+:: just pip list
+pip list --format=freeze > requirement.txt
+
+:: list all the environments.
+conda list
+
+:: remove the environment
+conda remove -n agentic_2_base --all 
+
+:: inkscape 
+inkscape input.svg --export-type=png
+
+
+
+
